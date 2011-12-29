7b40da0b54b612894657e629a3e44a5f96d952e4
Michael Garriss <mgarriss@gmail.com>
2011-12-29 15:07:25 -0800

    home_page_spec.rb has one example

---
+++ spec/integration/home_page_spec.rb
@@ -0,0 +1,10 @@
+describe "home page" do
+  it "diplays a list of selectable libraries if at least one exists" do
+    library = Factory(:library, name:"ableton live samples",
+                      path:"/private/tmp/ugly.live.specimen/test_library")
+    visit "/"
+    click_link "ableton live samples"
+    page.should have_selector('.library')
+    page.should have_selector('a[href="/"]')
+  end
+end
