4e323eaf3f3daa648d57897363a283e4977fa8ef
Michael Garriss <mgarriss@gmail.com>
2011-12-29 11:52:03 -0800

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UglyLiveSpecimen::Application.config.secret_token = '524940fddc2470498ae9f1a8803a94f62928500eaca68b57f67bc04360f64f1c51fcc54da76335229174497055f43bc7db4f2b73cc2a502cdcd7207be13a9dc4'
