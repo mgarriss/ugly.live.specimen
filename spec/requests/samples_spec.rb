919c22c07f1926a803d41b7b3f21ac6b3997b0cd
Michael Garriss <mgarriss@gmail.com>
2012-01-03 10:46:36 -0800

    clean up on samples_spec.rb

--- spec/requests/samples_spec.rb
+++ spec/requests/samples_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe "Samples" do
-  describe "GET /samples" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get samples_path
-      response.status.should be(200)
+describe "samples" do
+  describe "list" do
+    it "displays a list of selectable samples if at least one exists" do
+      sample = Factory(:sample)
+      visit "/samples"
+      page.should have_selector('.sample_summary')
     end
   end
 end
