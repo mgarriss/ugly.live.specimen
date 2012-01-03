0b568659c90ae67163b7bce428e05c6ce5d48c2d
Michael Garriss <mgarriss@gmail.com>
2012-01-03 10:50:46 -0800

    eliminate library concept

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 UglyLiveSpecimen::Application.routes.draw do
   resources :samples
 
-  resources :libraries
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +50,7 @@ UglyLiveSpecimen::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root to:redirect('/libraries')
+  root to:redirect('/samples')
 
   # See how all your routes lay out with "rake routes"
 
